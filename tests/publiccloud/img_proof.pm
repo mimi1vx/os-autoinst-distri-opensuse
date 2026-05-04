166cfbdd0475c6223bf9388cc8ae70d2fb6a29c9
Martin Loviška <mloviska@suse.com>
2026-05-04 12:37:38 +0200

    feat(img_proof): upload json and human readable logs
    The results of img_proof can uploaded in human readable format and as
    json. Make it also readable in the browser without the need to download
    the assets.
    
    Fix undef warning
    
    ```
    Printing analyze thingy run: $VAR1 = undef;
    Printing analyze thingy t: $VAR1 = 'test_soft_reboot';
    Use of uninitialized value in string ne at publiccloud/img_proof.pm
    ```

--- tests/publiccloud/img_proof.pm
+++ tests/publiccloud/img_proof.pm
@@ -68,7 +68,7 @@ sub analyze_results {
         my $json = Mojo::JSON::decode_json($file->slurp);
         my $logfile = path(bmwqemu::result_dir(), $json->{details}[0]->{text});
         for my $run (@runs) {
-            if ($run->{name} ne '' && index($t->{name}, $run->{name}) != -1) {
+            if ($run->{name} && index($t->{name}, $run->{name}) != -1) {
                 $logfile->append("\n\nimg-proof output:\n" . $run->{output});
                 $logfile->append("\n\nimg-proof log:\n" . $run->{log});
             }
@@ -137,8 +137,9 @@ sub run {
         patch_json $img_proof->{results} if (get_var('PUBLIC_CLOUD_SOFTFAIL_SCAP'));
     }
 
-    upload_logs($img_proof->{logfile}, log_name => basename($img_proof->{logfile}) . ".txt");
-
+    my $log_prefix = 'img_proof_log';
+    upload_logs($img_proof->{logfile}, log_name => sprintf('%s-%s.%s', $log_prefix, basename($img_proof->{logfile}), 'txt'));
+    upload_logs($img_proof->{results}, log_name => sprintf('%s-%s.%s', $log_prefix, basename($img_proof->{results}), 'json'));
     parse_extra_log(IPA => $img_proof->{results});
 
     $instance->ssh_script_run(cmd => 'sudo chmod a+r /var/tmp/report.html || true');
